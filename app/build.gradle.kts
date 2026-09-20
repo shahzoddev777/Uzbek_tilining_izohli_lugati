316d13d1c1a7dd25437a0ee93386bdc4af04cd9d
shahzodbek's laptop <shahzodbekreyimboyev022@gmail.com>
2026-09-20 20:40:30 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,17 +51,13 @@ dependencies {
 
     //room
     val room_version = "2.8.4"
-    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.room:room-runtime:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
-    implementation("net.datafaker:datafaker:2.5.4")
 
     //viewbinding
     implementation("dev.androidbroadcast.vbpd:vbpd:2.0.4")
     //splashscreen
     implementation("androidx.core:core-splashscreen:1.0.1")
-    //Material components
-    implementation("com.google.android.material:material:1.9.0")
     //notification
     implementation("androidx.work:work-runtime-ktx:2.9.1")
 }
